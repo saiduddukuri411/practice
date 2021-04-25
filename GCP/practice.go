1962aefd29e522f5dc5c701c65e3f2bc18569b3a
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2021-04-25 01:13:50 -0400

    added

---
+++ GCP/practice.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type shape interface {
+	area() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type rectangle struct {
+	width  float64
+	height float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * (math.Pow(c.radius, 2))
+}
+
+func (r rectangle) area() float64 {
+	return (r.width * r.height)
+}
+func main() {
+	c1 := circle{2}
+	r1 := rectangle{1, 2}
+	shapes := []shape{c1, r1}
+	fmt.Println(shapes[0].area())
+}
